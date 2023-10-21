d817519f6c28d10c4efb9c424416556d6da85ba9
Silvanors <silvanors@gmail.com>
2023-10-20 21:00:49 -0300

    Inicial

--- src/main/java/me/dio/santanderbootcamp2023/Application.java
+++ src/main/java/me/dio/santanderbootcamp2023/Application.java
@@ -5,6 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+	//editor de JSON on line "https://jsoneditoronline.org/"
+	//chatGPT para gerar os diagramas de classes UML "chat.openai.com"
+	//o código mermaid fornecido pelo chat pode ser usado como gráfico no github utilizando '''...'''
+	//pode ser consultado o site "mermaid.js.org" para conhecer mais
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
