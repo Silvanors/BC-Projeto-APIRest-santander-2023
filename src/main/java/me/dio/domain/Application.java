56770a220d783a6f595f5341c7f16c3e74d8e001
Silvano <silvanors@gmail.com>
2023-10-21 14:40:30 -0300

    Subindo modelagem das Classes

--- src/main/java/me/dio/domain/Application.java
+++ src/main/java/me/dio/domain/Application.java
@@ -1,4 +1,4 @@
-package me.dio.santanderbootcamp2023;
+package me.dio.domain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
